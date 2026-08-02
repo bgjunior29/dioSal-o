5bb86ad6c78ad74f9873d74803744ac51df1b6c5
bgjunior29 <jw5549846@gmail.com>
2026-08-02 12:54:15 -0300

    DionSalonv1.3

--- types/nodemailer.d.ts
+++ types/nodemailer.d.ts
@@ -1,1 +1,11 @@
 declare module "nodemailer";
+declare namespace NodeJS {
+  interface ProcessEnv {
+    DATABASE_URL?: string;
+    SMTP_HOST?: string;
+    SMTP_PORT?: string;
+    SMTP_USER?: string;
+    SMTP_PASS?: string;
+    SMTP_FROM?: string;
+  }
+}
